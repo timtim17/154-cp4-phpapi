ee53e3360e6530c27d1fdae67da33ae3f4384f1e
Austin Jenchi <ajenchi9@gmail.com>
2019-05-14 22:07:16 -0700

    fetch messages and add them to the page

--- main.js
+++ main.js
@@ -7,7 +7,9 @@
 (function() {
     "use strict";
 
+    const API_URL = "messageboard.php";
     const CLASS_ANIMATE = "animate";
+    const CLASS_MESSAGE = "message";
 
     window.addEventListener("load", init);
 
@@ -18,6 +20,38 @@
         qs("header").classList.add(CLASS_ANIMATE);
         qs("footer").classList.add(CLASS_ANIMATE);
         id("api-docs").classList.add(CLASS_ANIMATE);
+
+        fetch(API_URL)
+            .then(checkStatus)
+            .then(JSON.parse)
+            .then(populateMessages);
+    }
+
+    /**
+     * Using the response from the API add all of the messages to the page.
+     *
+     * @param {object} data - The response from the API
+     */
+    function populateMessages(data) {
+        data.forEach(msg => addMessage(msg));
+    }
+
+    /**
+     * Adds a message to the page. Creates an element for it and adds it as a child to the proper
+     * DOM node.
+     *
+     * @param {string} message - The message to add
+     */
+    function addMessage(message) {
+        let ele = document.createElement("section");
+        ele.classList.add(CLASS_MESSAGE);
+        ele.innerText = message;
+        let rotate = Math.random() * 20 - 10;
+        // animate the scaling of the element by setting a custom css property after a timeout
+        // the original value of this is set in the css - 0.0
+        ele.style.transform = "rotate(" + rotate + "deg) scale(var(--grow))";
+        qs("main").appendChild(ele);
+        setTimeout(() => ele.style.setProperty("--grow", 1), 2000);
     }
 
     /* CSE 154 HELPER FUNCTIONS */
