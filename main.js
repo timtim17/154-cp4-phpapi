8c0adfdb445251259aa95736e15ce5d8b68202b5
Austin Jenchi <ajenchi9@gmail.com>
2019-05-14 19:51:36 -0700

    Add basic HTML, CSS, and JS

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * @name Austin Jenchi
+ * CSE 154 19sp AQ
+ * @date 05/14/2019
+ * Basic Javascript to get messages from the backend and add them to the message board page.
+ */
+(function() {
+    "use strict";
+})();
